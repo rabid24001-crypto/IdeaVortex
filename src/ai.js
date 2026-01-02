317f51866be0034fc9467bf889f73de63adc0715
rabid24001-crypto <rabid.24.001@gmail.com>
2026-01-02 14:21:22 +0400

    Add generateIdeas function for content generation

---
+++ src/ai.js
@@ -0,0 +1,9 @@
+const generateIdeas = (topic) => {
+  return [
+    `Post about ${topic} for Instagram`,
+    `Tweet thread on ${topic}`,
+    `LinkedIn article about ${topic}`
+  ];
+};
+
+module.exports = { generateIdeas };
