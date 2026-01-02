5b6d40fa3a8cb9f52ab799858e590b32ca4612c5
rabid24001-crypto <rabid.24.001@gmail.com>
2026-01-02 14:19:37 +0400

    Delete Src directory

--- Src/ai.js
+++
@@ -1,9 +0,0 @@
-const generateIdeas = (topic) => {
-  return [
-    `Post about ${topic} for Instagram`,
-    `Tweet thread on ${topic}`,
-    `LinkedIn article about ${topic}`
-  ];
-};
-
-module.exports = { generateIdeas };
